dd5ae1bbca43d6790b0aa0fb80df0eae6d883e9c
Wade <ygh@gabia.com>
2021-07-02 11:15:42 +0900

    feat: commit test

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -9,6 +9,7 @@ public class DemoApplication {
     public static void main(String[] args) {
         System.out.println("husky test1");
         System.out.println("husky test2");
+        System.out.println("husky test3");
         SpringApplication.run(DemoApplication.class, args);
     }
 
