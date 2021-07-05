689687cda30d2c0e50cb8022c9abd04daf1ffcc8
Wade <ygh@gabia.com>
2021-07-05 09:09:52 +0900

    feat: header test"
    body test
    footer test

--- src/main/java/com/example/demo/AppConfig.java
+++ src/main/java/com/example/demo/AppConfig.java
@@ -1,2 +1,7 @@
-package com.example.demo;public class AppConfig {
+package com.example.demo;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
 }
--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,6 +2,8 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -10,7 +12,17 @@ public class DemoApplication {
         System.out.println("husky test1");
         System.out.println("husky test2");
         System.out.println("husky test3");
+
         SpringApplication.run(DemoApplication.class, args);
+
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        String[] beanNamesForType = ac.getBeanNamesForType(Object.class);
+
+        for (String s : beanNamesForType) {
+            System.out.println("name: " + s + " bean: " + ac.getBean(s));
+        }
+
     }
 
 }
